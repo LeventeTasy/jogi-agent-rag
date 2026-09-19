218491fff1d72ece7d3fd07c1def72b030b96bf8
Tasy Levente <tasy.levente2005@gmail.com>
2026-09-19 19:20:40 +0200

    modified evaluation script

--- test/scripts/evaluation.py
+++ test/scripts/evaluation.py
@@ -81,6 +81,6 @@ def evaluate(path1: str, path2: str):
 if __name__ == "__main__":
     BASE_DIR = Path(__file__).resolve().parent
     PATH1 = BASE_DIR.parent / "results" / "model_comparison" / "answered_questions_agent_base.csv"
-    PATH2 = BASE_DIR.parent / "results" / "model_comparison" / "answered_questions_agent_wo_ver.csv"
+    PATH2 = BASE_DIR.parent / "results" / "model_comparison" / "answered_questions_agent_wo_ver+gro.csv"
 
     evaluate(PATH1, PATH2)
